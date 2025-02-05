Scaling Fix

asparagus/data/datastats.py
@@ -394,6 +394,8 @@ def compute_atomic_property_sum_scaling(
     atoms_number: np.ndarray,
     atomic_numbers: np.ndarray,
     sys_i: np.ndarray,
+    set_shift_term: Optional[bool] = True,
+    set_scaling_factor: Optional[bool] = True,
     verbose: Optional[bool] = False,
 ) -> Dict[str, List[float]]:
     """
@@ -415,6 +417,10 @@ def compute_atomic_property_sum_scaling(
         System atomic numbers
     sys_i: np.ndarray
         System indices of atoms
+    set_shift_term: bool, optional, default True
+        If True, estimate shift terms.
+    set_scaling_factor: bool, optional, default True
+        If True, estimate the scaling factors.
 
     Return
     ------
@@ -458,21 +464,26 @@ def compute_atomic_property_sum_scaling(
         atomic_numbers, return_inverse=True)
 
     # Compute atomic energies average and standard deviation
-    property_atomic_reference_mean = np.mean(property_reference/atoms_number)
-    property_atomic_reference_stdv = np.std(
-        property_reference/atoms_number)
-    if property_atomic_reference_stdv == 0.0:
+    if set_shift_term:
+        property_atomic_reference_mean = np.mean(
+            property_reference/atoms_number)
+    else:
+        property_atomic_reference_mean = 0.0
+    if set_scaling_factor:
+        property_atomic_reference_stdv = np.std(
+            property_reference/atoms_number)
+    elif not set_scaling_factor or property_atomic_reference_stdv == 0.0:
         property_atomic_reference_stdv = 1.0
 
     # Set initial property shift and scaling parameter as atom scaled average
     # and standard deviation of the system reference property
-    property_scale = np.full(
-        atomic_numbers_list.shape,
-        property_atomic_reference_stdv,
-        dtype=float)
     property_shift = np.full(
         atomic_numbers_list.shape,
         property_atomic_reference_mean,
+        dtype=float)
+    property_scale = np.full(
+        atomic_numbers_list.shape,
+        property_atomic_reference_stdv,
         dtype=float)
 
     if property_prediction_scaling is None:
@@ -490,19 +501,19 @@ def compute_atomic_property_sum_scaling(
         # Iterate over available elements
         for iatom, atomic_number in enumerate(atomic_numbers_list):
 
+            selection = atomic_number==atomic_numbers
             property_atomic_prediction_shift = np.mean(
-                property_prediction[atomic_number==atomic_numbers])
+                property_prediction[selection])
             property_atomic_prediction_scale = np.std(
-                property_prediction[atomic_number==atomic_numbers])
+                property_prediction[selection])
             if property_atomic_prediction_scale == 0.0:
                 property_atomic_prediction_scale = 1.0
-            #print(iatom, atomic_number, property_atomic_prediction_scale, property_atomic_prediction_shift)
+
             # Attune atomic property shift and scaling parameter
-            #print(property_scale[iatom], property_shift[iatom])
-            property_scale[iatom] /= property_atomic_prediction_scale
-            property_shift[iatom] -= (
-                property_atomic_prediction_shift*property_scale[iatom])
-            #print(property_scale[iatom], property_shift[iatom])
+            if set_shift_term:
+                property_shift[iatom] -= property_atomic_prediction_shift
+            if set_scaling_factor:
+                property_scale[iatom] /= property_atomic_prediction_scale
 
     # Define energy computation and evaluation function
     def property_sum(
@@ -520,10 +531,11 @@ def compute_atomic_property_sum_scaling(
         if property_prediction is None:
             atomic_property = np.array(shifts)[aidcs]
         else:
-            atomic_property = (
-                (property_prediction[aidcs] - model_scaling[aidcs, 0])
-                * scales[aidcs]
-                + shifts[aidcs] + model_scaling[aidcs, 0])
+            atomic_property = property_prediction.copy()
+            for iatom, atomic_number in enumerate(atomic_numbers_list):
+                selection = atomic_number==atomic_numbers
+                atomic_property[selection] = (
+                    atomic_property[selection]*scales[iatom] + shifts[iatom])
 
         # Sum up atomic property to system property
         np.add.at(prediction, sidcs, atomic_property)
@@ -537,7 +549,14 @@ def compute_atomic_property_sum_scaling(
     ):
 
         # Collect property per atom type
-        shifts, scales = pars.reshape(2, -1)
+        if set_shift_term and set_scaling_factor:
+            shifts, scales = pars.reshape(2, -1)
+        elif set_shift_term:
+            shifts = pars
+            scales = np.ones_like(pars)
+        else:
+            scales = pars
+            shifts = np.zeros_like(pars)
         prediction = property_sum(shifts, scales)
 
         # Compute root mean square error between reference and prediction
@@ -548,15 +567,23 @@ def compute_atomic_property_sum_scaling(
 
         return rmse
 
-    # If systems have different atom compositions,
+    # If systems have different atom compositions or prediction is available,
     # fit atomic property to match best the system energies.
-    if various_systems:
+    if (
+        (various_systems or property_prediction is not None)
+        and (set_shift_term or set_scaling_factor)
+    ):
 
         # Start fitting procedure
         try:
 
             # Concatenate shift term and scaling factor
-            pars = np.append(property_shift, property_scale)
+            if set_shift_term and set_scaling_factor:
+                pars = np.append(property_shift, property_scale)
+            elif set_shift_term:
+                pars = property_shift
+            else:
+                pars = property_scale
 
             # Start fitting atomic energies shifts
             from scipy.optimize import minimize
@@ -567,7 +594,12 @@ def compute_atomic_property_sum_scaling(
             fit_rmse = fit_result.fun
 
             # Assign fit results
-            property_shift, property_scale = fit_result.x.reshape(2, -1)
+            if set_shift_term and set_scaling_factor:
+                property_shift, property_scale = fit_result.x.reshape(2, -1)
+            elif set_shift_term:
+                property_shift = fit_result.x.reshape(-1)
+            else:
+                property_scale = fit_result.x.reshape(-1)
 
             # Set successful fitting flag
             fit_complete = True
@@ -585,9 +617,6 @@ def compute_atomic_property_sum_scaling(
         # Set None fitting flag
         fit_complete = None
         fit_rmse = np.nan
-
-    #pars = np.append(property_shift, property_scale)
-    #print(system_property_eval(pars))
 
     # Assign atomic energies statistics
     for iatom, atomic_number in enumerate(atomic_numbers_list):

asparagus/training/scaling.py
@@ -99,7 +99,9 @@ def set_property_scaling_estimation(
             properties_first,
             use_model_prediction,
             model_conversion,
-            atomic_energies_guess)
+            atomic_energies_guess,
+            set_shift_term=set_shift_term,
+            set_scaling_factor=set_scaling_factor)
 
         # Set model property scaling parameter to the model calculator output
         # module
@@ -122,7 +124,9 @@ def set_property_scaling_estimation(
             properties_second,
             use_model_prediction,
             model_conversion,
-            atomic_energies_guess)
+            atomic_energies_guess,
+            set_shift_term=set_shift_term,
+            set_scaling_factor=set_scaling_factor)
 
         # Set model property scaling parameter to the model calculator output
         # module
@@ -146,6 +150,8 @@ def estimate_property_scaling(
     use_model_prediction: bool,
     model_conversion: Dict[str, float],
     atomic_energies_guess: bool,
+    set_shift_term: Optional[bool] = True,
+    set_scaling_factor: Optional[bool] = True,
 ) -> Dict[str, Union[List[float], Dict[int, List[float]]]]:
     """
     Estimate model property scaling parameters and prepare respective
@@ -170,6 +176,10 @@ def estimate_property_scaling(
         In case atomic energies are not available by the reference data
         set, which is normally the case, predict anyways from a guess of
         the difference between total reference energy and predicted energy.
+    set_shift_term: bool, optional, default True
+        If True, estimate shift terms.
+    set_scaling_factor: bool, optional, default True
+        If True, estimate the scaling factors.
 
     Returns
     -------
@@ -216,7 +226,9 @@ def estimate_property_scaling(
         properties_reference,
         properties_prediction,
         model_scaling,
-        atomic_energies_guess)
+        atomic_energies_guess,
+        set_shift_term=set_shift_term,
+        set_scaling_factor=set_scaling_factor)
 
     return property_scaling
 
@@ -282,9 +294,6 @@ def get_model_prediction_and_reference_properties(
         for prop in properties_system + properties_available:
             properties_reference[prop].append(
                 batch[prop].cpu().detach())
-
-        #if ib > 1:
-            #break
 
     # Concatenate prediction and reference results
     for prop, item in properties_prediction.items():
@@ -390,6 +399,8 @@ def compute_property_scaling(
     properties_prediction: Dict[str, np.ndarray],
     model_scaling: Dict[str, Union[List[float], Dict[int, List[float]]]],
     atomic_energies_guess: bool,
+    set_shift_term: Optional[bool] = True,
+    set_scaling_factor: Optional[bool] = True,
 ) -> Dict[str, Union[List[float], Dict[int, List[float]]]]:
     """
     Compute guess of property scaling parameters either from the reference
@@ -406,6 +417,10 @@ def compute_property_scaling(
     atomic_energies_guess: bool
         Predict atomic energies scaling parameter eventually from the
         total system energy data.
+    set_shift_term: bool, optional, default True
+        If True, estimate shift terms.
+    set_scaling_factor: bool, optional, default True
+        If True, estimate the scaling factors.
 
     Returns
     -------
@@ -451,18 +466,23 @@ def compute_property_scaling(
                 # Special case: Atomic energies scaling guess
                 if prop == 'energy' and atomic_energies_guess:
 
-                    # Prepare reference energy to scale output module atomic
+                    # Prepare reference energy by subtracting energy 
+                    # contribution which are not from the output module atomic
                     # energies
                     reference_output_energy = (
                         properties_reference['energy'].copy())
-                    property_tag = 'energy'
+                    property_tag = '_energy'
+                    property_list = []
                     for prop, item in properties_prediction.items():
                         if property_tag in prop[-len(property_tag):]:
                             if 'output_' in prop:
                                 continue
                             else:
+                                property_list.append(prop)
                                 reference_output_energy -= item
 
+                    # Scale atomic energies output to best match total system
+                    # energy
                     properties_scaling['atomic_energies'] = (
                         data.compute_atomic_property_sum_scaling(
                             reference_output_energy,
@@ -471,7 +491,9 @@ def compute_property_scaling(
                             model_scaling['atomic_energies'],
                             properties_reference['atoms_number'],
                             properties_reference['atomic_numbers'],
-                            properties_reference['sys_i'])
+                            properties_reference['sys_i'],
+                            set_shift_term=set_shift_term,
+                            set_scaling_factor=set_scaling_factor)
                         )
 
                 elif prop == 'atomic_energies':
@@ -508,12 +530,16 @@ def compute_property_scaling(
         if prop in properties_scaling:
             if utils.is_dictionary(properties_scaling[prop]):
                 for ai in properties_scaling[prop]:
-                    properties_scaling[prop][ai][0] += (
-                        model_scaling[prop][ai][0])
+                    properties_scaling[prop][ai][0] = (
+                        model_scaling[prop][ai][0]
+                        * properties_scaling[prop][ai][1]
+                        + properties_scaling[prop][ai][0])
                     properties_scaling[prop][ai][1] *= (
                         model_scaling[prop][ai][1])
             else:
-                properties_scaling[prop][0] += model_scaling[prop][0]
+                properties_scaling[prop][0] = (
+                    model_scaling[prop][0]*properties_scaling[prop][1]
+                    + properties_scaling[prop][0])
                 properties_scaling[prop][1] *= model_scaling[prop][1]
 
     return properties_scaling